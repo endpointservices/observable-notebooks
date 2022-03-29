79f02bc1ba622cc01edd005fc9db3226b15fee6c
backup-to-github <robot@webcode.run>
2022-03-29 07:40:20 +0000

    Backup https://observablehq.com/@endpointservices/puppeteer   url:     https://observablehq.com/@endpointservices/puppeteer   title:   Browser Automation with Puppeteer   author:  Endpoint Services (@endpointservices)   id:      e806bf1960f4aca8   version: 443

--- @endpointservices/puppeteer/e806bf1960f4aca8@443.js
+++ @endpointservices/puppeteer/e806bf1960f4aca8@443.js
@@ -1,13 +1,9 @@
 // https://observablehq.com/@endpointservices/puppeteer@443
-import define1 from "./f92778131fd76559@1169.js";
+import define1 from "./f92778131fd76559@1173.js";
 import define2 from "./c16efae137e70585@1416.js";
 import define3 from "./293899bef371e135@216.js";
 
-export default function define(runtime, observer) {
-  const main = runtime.module();
-  const fileAttachments = new Map([["puppeteer-web-0.0.3.js",new URL("./files/d388b40ec6f0de2f7bc7298b068e8a0e84b4db2a6f31d6e150901f185e40f04c3bd38d06b75ada5932ece93d6e1af1fed8a9a6369d0682ae790944f964e9d924",import.meta.url)],["puppeteer.png",new URL("./files/243b533b7711810576f16d3718a988783a7b4cfc059c2eda243baf41d26762d9f128c746a5a8c2b181c7b7895506df8de83dd693447949a579543c0a91e969e4",import.meta.url)]]);
-  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer()).define(["md","FileAttachment"], async function(md,FileAttachment){return(
+async function _1(md,FileAttachment){return(
 md`# Browser Automation with Puppeteer
 
 ![](${await FileAttachment("puppeteer.png").url()})
@@ -55,11 +51,13 @@ Most things that you can do manually in the browser can be done using Puppeteer!
 
 
 `
-)});
-  main.variable(observer()).define(["viewof user"], function($0){return(
+)}
+
+function _2($0){return(
 $0
-)});
-  main.variable(observer("viewof connect")).define("viewof connect", ["md","user","puppeteer_web"], function(md,user,puppeteer_web)
+)}
+
+function _connect(md,user,puppeteer_web)
 {
   const ui = md`
 <details>
@@ -78,12 +76,13 @@ account ${user.uid}
     }
   return ui;
 }
-);
-  main.variable(observer("connect")).define("connect", ["Generators", "viewof connect"], (G, _) => G.input(_));
-  main.variable(observer()).define(["md"], function(md){return(
+
+
+function _4(md){return(
 md`### Taking a screenshot`
-)});
-  main.variable(observer("screenshot")).define("screenshot", ["runDemos","connect","htl"], async function(runDemos,connect,htl)
+)}
+
+async function _screenshot(runDemos,connect,htl)
 {
   runDemos;
   const browser = await connect();
@@ -97,11 +96,13 @@ md`### Taking a screenshot`
   browser.close(); // Usage based pricing! Close when not in use!!!
   return img;
 }
-);
-  main.variable(observer()).define(["md"], function(md){return(
+
+
+function _6(md){return(
 md`### Scraping the DOM`
-)});
-  main.variable(observer("scrapeDOM")).define("scrapeDOM", ["runDemos","connect","Inputs"], async function(runDemos,connect,Inputs)
+)}
+
+async function _scrapeDOM(runDemos,connect,Inputs)
 {
   runDemos;
   const browser = await connect();
@@ -123,11 +124,13 @@ md`### Scraping the DOM`
     layout: "auto"
   });
 }
-);
-  main.variable(observer()).define(["md"], function(md){return(
+
+
+function _8(md){return(
 md`## Performance Tracing`
-)});
-  main.variable(observer()).define(["Plot","completeEvents"], function(Plot,completeEvents){return(
+)}
+
+function _9(Plot,completeEvents){return(
 Plot.plot({
   y: {
     axis: null
@@ -141,8 +144,9 @@ Plot.plot({
     })
   ]
 })
-)});
-  main.variable(observer("traceData")).define("traceData", ["runDemos","connect"], async function(runDemos,connect)
+)}
+
+async function _traceData(runDemos,connect)
 {
   runDemos
   const browser = await connect()
@@ -154,16 +158,18 @@ Plot.plot({
   await browser.close();
   return buffer
 }
-);
-  main.variable(observer()).define(["htl","traceData"], function(htl,traceData){return(
+
+
+function _11(htl,traceData){return(
 htl.html`
 
 
 <a download="profile.json" href="${URL.createObjectURL(new Blob([JSON.stringify(JSON.parse(traceData))], {type: "application/json"}))}"> download profile data</a>
 <p>Its compatible with <a href="https://chromedevtools.github.io/timeline-viewer/">timeline-viewer</a>
 `
-)});
-  main.variable(observer("completeEvents")).define("completeEvents", ["traceData"], function(traceData)
+)}
+
+function _completeEvents(traceData)
 {
   let depth = 0;
   return JSON.parse(traceData).traceEvents.reduce(
@@ -184,13 +190,39 @@ htl.html`
   , [])
 
 }
-);
-  main.variable(observer("runDemos")).define("runDemos", ["htl","invalidation"], function(htl,invalidation){return(
+
+
+function _runDemos(htl,invalidation){return(
 htl.html`<a href="">`.href.startsWith('https://observablehq.com/@endpointservices/puppeteer') || invalidation
-)});
-  main.variable(observer("puppeteer_web")).define("puppeteer_web", ["require","FileAttachment"], async function(require,FileAttachment){return(
+)}
+
+async function _puppeteer_web(require,FileAttachment){return(
 require(await FileAttachment("puppeteer-web-0.0.3.js").url())
-)});
+)}
+
+function _18(footer){return(
+footer
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  const fileAttachments = new Map([["puppeteer-web-0.0.3.js",new URL("./files/d388b40ec6f0de2f7bc7298b068e8a0e84b4db2a6f31d6e150901f185e40f04c3bd38d06b75ada5932ece93d6e1af1fed8a9a6369d0682ae790944f964e9d924",import.meta.url)],["puppeteer.png",new URL("./files/243b533b7711810576f16d3718a988783a7b4cfc059c2eda243baf41d26762d9f128c746a5a8c2b181c7b7895506df8de83dd693447949a579543c0a91e969e4",import.meta.url)]]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md","FileAttachment"], _1);
+  main.variable(observer()).define(["viewof user"], _2);
+  main.variable(observer("viewof connect")).define("viewof connect", ["md","user","puppeteer_web"], _connect);
+  main.variable(observer("connect")).define("connect", ["Generators", "viewof connect"], (G, _) => G.input(_));
+  main.variable(observer()).define(["md"], _4);
+  main.variable(observer("screenshot")).define("screenshot", ["runDemos","connect","htl"], _screenshot);
+  main.variable(observer()).define(["md"], _6);
+  main.variable(observer("scrapeDOM")).define("scrapeDOM", ["runDemos","connect","Inputs"], _scrapeDOM);
+  main.variable(observer()).define(["md"], _8);
+  main.variable(observer()).define(["Plot","completeEvents"], _9);
+  main.variable(observer("traceData")).define("traceData", ["runDemos","connect"], _traceData);
+  main.variable(observer()).define(["htl","traceData"], _11);
+  main.variable(observer("completeEvents")).define("completeEvents", ["traceData"], _completeEvents);
+  main.variable(observer("runDemos")).define("runDemos", ["htl","invalidation"], _runDemos);
+  main.variable(observer("puppeteer_web")).define("puppeteer_web", ["require","FileAttachment"], _puppeteer_web);
   const child1 = runtime.module(define1);
   main.import("view", child1);
   const child2 = runtime.module(define2);
@@ -198,8 +230,6 @@ require(await FileAttachment("puppeteer-web-0.0.3.js").url())
   main.import("user", child2);
   const child3 = runtime.module(define3);
   main.import("footer", child3);
-  main.variable(observer()).define(["footer"], function(footer){return(
-footer
-)});
+  main.variable(observer()).define(["footer"], _18);
   return main;
 }
