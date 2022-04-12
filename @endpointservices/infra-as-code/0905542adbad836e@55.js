50c4b2fb5883b711634635fe6d6594c6da635d5f
backup-to-github <robot@webcode.run>
2022-04-12 09:27:57 +0000

    Backup https://observablehq.com/@endpointservices/infra-as-code   url:     https://observablehq.com/@endpointservices/infra-as-code   title:   Declarative Infra-as-code Helpers   author:  Endpoint Services (@endpointservices)   id:      1956899a56408464   version: 459

--- @endpointservices/infra-as-code/0905542adbad836e@55.js
+++ @endpointservices/infra-as-code/0905542adbad836e@55.js
@@ -1,12 +1,11 @@
 // https://observablehq.com/@mbostock/hello-lodash-fp@55
-export default function define(runtime, observer) {
-  const main = runtime.module();
-  main.variable(observer()).define(["md"], function(md){return(
+function _1(md){return(
 md`# Hello, lodash/fp
 
 lodash doesn’t publish [lodash.fp.min.js to npm](https://github.com/lodash/lodash/issues/4014), and [the documentation](https://github.com/lodash/lodash/wiki/FP-Guide) recommends an undocumented, deprecated jsDelivr API (/g/). Instead, lodash publishes this file to GitHub Pages, and is available on jsDeliver under /gh/. The provided AMD exports a function (*fp*) which when called will redefine the lodash object (*_*) with functional equivalents.`
-)});
-  main.variable(observer("_")).define("_", ["require"], async function(require)
+)}
+
+async function __(require)
 {
   const [_, fp] = await Promise.all([
     require("lodash@4"),
@@ -14,9 +13,16 @@ lodash doesn’t publish [lodash.fp.min.js to npm](https://github.com/lodash/lod
   ]);
   return fp(_);
 }
-);
-  main.variable(observer()).define(["_"], function(_){return(
+
+
+function _3(_){return(
 _.defaults({ 'a': 2, 'b': 2 })({ 'a': 1 })
-)});
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("_")).define("_", ["require"], __);
+  main.variable(observer()).define(["_"], _3);
   return main;
 }
