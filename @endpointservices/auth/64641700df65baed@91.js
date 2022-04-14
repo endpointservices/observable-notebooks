dd723b59480b7a65e65bf57755871182ada7210e
backup-to-github <robot@webcode.run>
2022-04-14 01:43:47 +0000

    Backup https://observablehq.com/@endpointservices/auth   url:     https://observablehq.com/@endpointservices/auth   title:   Federated IndieAuth Server   author:  Endpoint Services (@endpointservices)   id:      e657c642789a0b68   version: 2027

--- @endpointservices/auth/64641700df65baed@91.js
+++ @endpointservices/auth/64641700df65baed@91.js
@@ -1,7 +1,5 @@
 // https://observablehq.com/@bryangingechen/toc@91
-export default function define(runtime, observer) {
-  const main = runtime.module();
-  main.variable(observer()).define(["md"], function(md){return(
+function _1(md){return(
 md`# TOC
 
 This notebook can generate a table of contents automatically for your notebook.
@@ -11,14 +9,17 @@ import {toc} from "@bryangingechen/toc"
 \`\`\`
 
 Here’s an example:`
-)});
-  main.variable(observer()).define(["toc"], function(toc){return(
+)}
+
+function _2(toc){return(
 toc()
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _3(md){return(
 md`## Implementation`
-)});
-  main.variable(observer("toc")).define("toc", ["Generators","html","DOM","MutationObserver"], function(Generators,html,DOM,MutationObserver){return(
+)}
+
+function _toc(Generators,html,DOM,MutationObserver){return(
 function toc(selector = "h1,h2,h3,h4,h5,h6") {
   return Generators.observe(notify => {
     let headings = [];
@@ -42,29 +43,50 @@ function toc(selector = "h1,h2,h3,h4,h5,h6") {
     return () => observer.disconnect();
   });
 }
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _5(md){return(
 md`## Hooray
 
 It worked!`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _6(md){return(
 md`### This is a sub-section`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _7(md){return(
 md`A little text`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _8(md){return(
 md`#### A sub-sub-section?`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _9(md){return(
 md`##### A sub-sub-sub-section?`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _10(md){return(
 md`###### A sub-sub-sub-sub-section! (h6!)`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _11(md){return(
 md`### Another sub-section`
-)});
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["toc"], _2);
+  main.variable(observer()).define(["md"], _3);
+  main.variable(observer("toc")).define("toc", ["Generators","html","DOM","MutationObserver"], _toc);
+  main.variable(observer()).define(["md"], _5);
+  main.variable(observer()).define(["md"], _6);
+  main.variable(observer()).define(["md"], _7);
+  main.variable(observer()).define(["md"], _8);
+  main.variable(observer()).define(["md"], _9);
+  main.variable(observer()).define(["md"], _10);
+  main.variable(observer()).define(["md"], _11);
   return main;
 }
