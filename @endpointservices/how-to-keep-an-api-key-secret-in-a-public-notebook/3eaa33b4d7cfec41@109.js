1fa26e1d7c0e4545f464c8cafe0abf6653c4a8d1
backup-to-github <robot@webcode.run>
2022-05-05 07:05:24 +0000

    Backup https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook   url:     https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook   title:   How To Keep an API key secret in a Public Notebook   author:  Endpoint Services (@endpointservices)   id:      3eaa33b4d7cfec41   version: 109

--- @endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook/3eaa33b4d7cfec41@109.js
+++ @endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook/3eaa33b4d7cfec41@109.js
@@ -1,12 +1,8 @@
 // https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook@109
-import define1 from "./dff1e917c89f5e76@1709.js";
-import define2 from "./293899bef371e135@216.js";
+import define1 from "./dff1e917c89f5e76@1711.js";
+import define2 from "./293899bef371e135@226.js";
 
-export default function define(runtime, observer) {
-  const main = runtime.module();
-  const fileAttachments = new Map([["image@1.png",new URL("./files/d5654fa8fc805df282a4ae6cbb5027f47e1bc44de10de639869ba77eb130ca96c7f842b8776e23d586baa3dc26c3f5e04e5ad5eaea8f4cd14f013bfab0920141",import.meta.url)]]);
-  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
-  main.variable(observer()).define(["md"], function(md){return(
+function _1(md){return(
 md`# How To Keep an API key secret in a Public Notebook
 
 <mark>I now recommend using the <a href="https://observablehq.com/@endpointservices/public-api-keys">webcode</a> dashboard for setting secrets</mark>
@@ -16,16 +12,18 @@ You can keep a secret in a public notebook by using a combination of [Secret Man
 motivation:
 https://talk.observablehq.com/t/is-it-possible-to-encapsulate-secret-based-code-for-public-notebook-use/4649
 `
-)});
-  main.variable(observer()).define(["md","FileAttachment"], async function(md,FileAttachment){return(
+)}
+
+async function _2(md,FileAttachment){return(
 md`
 ## Save your secret to [Secret Manager](https://observablehq.com/@tomlarkworthy/secret-manager)
 
 
 ![](${await FileAttachment("image@1.png").url()})
 `
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _3(md){return(
 md`## Inject secret into serverless cell handler that calls the protected API
 
 In the deploy options, tell the runtime to inject your secret. In the handler, you can retreive the secret in the runtime context.
@@ -33,8 +31,9 @@ In the deploy options, tell the runtime to inject your secret. In the handler, y
 Then call the API using the secret, and pipe the result to the response of the serverless-cell.
 
 `
-)});
-  main.variable(observer("proxy")).define("proxy", ["deploy"], function(deploy){return(
+)}
+
+function _proxy(deploy){return(
 deploy(
   "proxy",
   async (request, response, context) => {
@@ -61,43 +60,71 @@ deploy(
     secrets: ["endpointservices_example_secret"]
   }
 )
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _5(md){return(
 md`## PUBLISH YOUR NOTEBOOK
 
 The Serverless cell runtime needs to be able to access your notebook to read the handler code. So you must link share or publish your notebook. 
 `
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _6(md){return(
 md`## Try the link manually
 
 The link returned by the _deploy_ command is one you can click on. (though it won't have URL parameters)
 `
-)});
-  main.variable(observer()).define(["html","proxy"], function(html,proxy){return(
+)}
+
+function _7(html,proxy){return(
 html`${proxy}`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _8(md){return(
 md`## Call the link programatically
 
 (of course I don't have access to a real API_KEY so it breaks with 500)
 `
-)});
-  main.variable(observer("proxyResponse")).define("proxyResponse", ["proxy"], function(proxy){return(
+)}
+
+function _proxyResponse(proxy){return(
 fetch(`${proxy.href}?start=20200101&end=20200201`)
-)});
-  main.variable(observer()).define(["proxyResponse"], function(proxyResponse){return(
+)}
+
+function _10(proxyResponse){return(
 proxyResponse.text()
-)});
-  main.variable(observer()).define(["proxyResponse"], function(proxyResponse){return(
+)}
+
+function _11(proxyResponse){return(
 proxyResponse.status
-)});
+)}
+
+function _14(footer){return(
+footer
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  function toString() { return this.url; }
+  const fileAttachments = new Map([
+    ["image@1.png", {url: new URL("./files/d5654fa8fc805df282a4ae6cbb5027f47e1bc44de10de639869ba77eb130ca96c7f842b8776e23d586baa3dc26c3f5e04e5ad5eaea8f4cd14f013bfab0920141", import.meta.url), mimeType: "image/png", toString}]
+  ]);
+  main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["md","FileAttachment"], _2);
+  main.variable(observer()).define(["md"], _3);
+  main.variable(observer("proxy")).define("proxy", ["deploy"], _proxy);
+  main.variable(observer()).define(["md"], _5);
+  main.variable(observer()).define(["md"], _6);
+  main.variable(observer()).define(["html","proxy"], _7);
+  main.variable(observer()).define(["md"], _8);
+  main.variable(observer("proxyResponse")).define("proxyResponse", ["proxy"], _proxyResponse);
+  main.variable(observer()).define(["proxyResponse"], _10);
+  main.variable(observer()).define(["proxyResponse"], _11);
   const child1 = runtime.module(define1);
   main.import("deploy", child1);
   const child2 = runtime.module(define2);
   main.import("footer", child2);
-  main.variable(observer()).define(["footer"], function(footer){return(
-footer
-)});
+  main.variable(observer()).define(["footer"], _14);
   return main;
 }
