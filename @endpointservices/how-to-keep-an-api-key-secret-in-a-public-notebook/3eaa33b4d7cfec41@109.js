8a9793ea1c290b38f6aacdfe1dfcd36239395960
backup-to-github <robot@webcode.run>
2022-05-25 10:47:47 +0000

    Backup https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook   url:     https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook   title:   How To Keep an API key secret in a Public Notebook   author:  Endpoint Services (@endpointservices)   id:      3eaa33b4d7cfec41   version: 109

--- @endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook/3eaa33b4d7cfec41@109.js
+++ @endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook/3eaa33b4d7cfec41@109.js
@@ -1,6 +1,6 @@
 // https://observablehq.com/@endpointservices/how-to-keep-an-api-key-secret-in-a-public-notebook@109
 import define1 from "./dff1e917c89f5e76@1711.js";
-import define2 from "./293899bef371e135@226.js";
+import define2 from "./293899bef371e135@247.js";
 
 function _1(md){return(
 md`# How To Keep an API key secret in a Public Notebook
