27d31fb3dbea5ae19184cdc539959a3153342dc0
backup-to-github <robot@webcode.run>
2022-05-25 10:45:37 +0000

    Backup https://observablehq.com/@endpointservices/deploy-and-test-latency   url:     https://observablehq.com/@endpointservices/deploy-and-test-latency   title:   Deploy and Test webcode workflow latency measure   author:  Endpoint Services (@endpointservices)   id:      13a525f8b7fff694   version: 180

--- @endpointservices/deploy-and-test-latency/13a525f8b7fff694@180.js
+++ @endpointservices/deploy-and-test-latency/13a525f8b7fff694@180.js
@@ -1,42 +1,44 @@
-import define1 from "./6eda90668ae03044@803.js";
-import define2 from "./293899bef371e135@216.js";
+import define1 from "./6eda90668ae03044@804.js";
+import define2 from "./293899bef371e135@247.js";
 
-export default function define(runtime, observer) {
-  const main = runtime.module();
-  main.variable(observer()).define(["md"], function(md){return(
+function _1(md){return(
 md`# Deploy and Test webcode workflow latency measure
 
 Here we test out how fast new endpoint code can be observed on the public internet. Note the insants deploy technology requires a developer to be logged in, so if you want to run this experiment yourself you will need to fork into your own namespace first! In Berlin I get a latency of 130*ms* to deploy and test an endpoint. This is literally an upper bound on how long it takes for a code change to be live-on-prod!`
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _2(md){return(
 md`#### mutable t_start
 
 *t_start* is a mutable variable at the root of the dataflow dependency graph. If *t_start* changes, everything else underneath is recomputed to use its new value. Because it's marked mutable, we are able to programatically update its value from anywhere else in the notebook.`
-)});
-  main.define("initial t_start", function(){return(
+)}
+
+function _t_start(){return(
 Date.now()
-)});
-  main.variable(observer("mutable t_start")).define("mutable t_start", ["Mutable", "initial t_start"], (M, _) => new M(_));
-  main.variable(observer("t_start")).define("t_start", ["mutable t_start"], _ => _.generator);
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _4(md){return(
 md`#### endpoint
 
 our *endpoint* cell prints the value of *t_start* and therefore depends on it. Thus, when *t_start* changes, *endpoint* redeploys and so subsequent calls to the endpoint will include the new *t_start* value`
-)});
-  main.variable(observer("endpoint")).define("endpoint", ["deploy","t_start"], function(deploy,t_start){return(
+)}
+
+function _endpoint(deploy,t_start){return(
 deploy("latency", (req, res) =>
   res.json({
     t_start: t_start, // we depend on the hardcoded value of t_start
     t_handler: Date.now()
   })
 )
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _6(md){return(
 md`### Test
 
 Finally we make a test routine depend on the *endpoint* cell, so that after a redeploy, a call is made to the endpoint immediately. We confirm the value returned from the endpoint matches our session's t_start, and measure the end-to-end latency between t_start and the endpoint response. This will obviously include network latency (check you devtool network log to see the requests). We add that leatency measure to a dataset for visualization.`
-)});
-  main.variable(observer("test")).define("test", ["endpoint","t_start","mutable data"], async function(endpoint,t_start,$0)
+)}
+
+async function _test(endpoint,t_start,$0)
 {
   // test depends on "endpoint" cell so it auto run after "endpoint" is redeployed
   const response = await fetch(endpoint.href);
@@ -57,43 +59,67 @@ Finally we make a test routine depend on the *endpoint* cell, so that after a re
   ];
   return timing;
 }
-);
-  main.variable(observer()).define(["md"], function(md){return(
+
+
+function _8(md){return(
 md`## Run an experiment
 
 By mutating *t_start*, we cause a chain of reevaluations through the dataflow graph
 - *t_start* is updated
 - *endpoint* is reployed and starts serving the new value to *t_start*
 - *test* calls *endpoint* and measures the delta between *t_start* and when it has a response.`
-)});
-  main.variable(observer()).define(["Inputs","mutable t_start"], function(Inputs,$0){return(
+)}
+
+function _9(Inputs,$0){return(
 Inputs.button("redeploy and test", {
   // changing t_start invalidates the handler triggering a redploy
   reduce: () => ($0.value = Date.now())
 })
-)});
-  main.variable(observer()).define(["md"], function(md){return(
+)}
+
+function _10(md){return(
 md`### Roundrip latency to redeploy and call an endpoint`
-)});
-  main.variable(observer()).define(["Plot","data"], function(Plot,data){return(
+)}
+
+function _11(Plot,data){return(
 Plot.plot({
   y: {
     label: "Latency (ms)"
   },
   marks: [Plot.barY(data, { x: (d, i) => i, y: "roundtrip" })]
 })
-)});
-  main.define("initial data", function(){return(
+)}
+
+function _data(){return(
 []
-)});
+)}
+
+function _15(footer){return(
+footer
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["md"], _2);
+  main.define("initial t_start", _t_start);
+  main.variable(observer("mutable t_start")).define("mutable t_start", ["Mutable", "initial t_start"], (M, _) => new M(_));
+  main.variable(observer("t_start")).define("t_start", ["mutable t_start"], _ => _.generator);
+  main.variable(observer()).define(["md"], _4);
+  main.variable(observer("endpoint")).define("endpoint", ["deploy","t_start"], _endpoint);
+  main.variable(observer()).define(["md"], _6);
+  main.variable(observer("test")).define("test", ["endpoint","t_start","mutable data"], _test);
+  main.variable(observer()).define(["md"], _8);
+  main.variable(observer()).define(["Inputs","mutable t_start"], _9);
+  main.variable(observer()).define(["md"], _10);
+  main.variable(observer()).define(["Plot","data"], _11);
+  main.define("initial data", _data);
   main.variable(observer("mutable data")).define("mutable data", ["Mutable", "initial data"], (M, _) => new M(_));
   main.variable(observer("data")).define("data", ["mutable data"], _ => _.generator);
   const child1 = runtime.module(define1);
   main.import("deploy", child1);
   const child2 = runtime.module(define2);
   main.import("footer", child2);
-  main.variable(observer()).define(["footer"], function(footer){return(
-footer
-)});
+  main.variable(observer()).define(["footer"], _15);
   return main;
 }
