009a85d1a87ba31037c7888b18a9230104798bf2
backup-to-github <robot@webcode.run>
2022-06-17 05:19:37 +0000

    Backup https://observablehq.com/@endpointservices/footer-with-backups   url:     https://observablehq.com/@endpointservices/footer-with-backups   title:   Endpoint Services Footer   author:  Endpoint Services (@endpointservices)   id:      293899bef371e135   version: 208

--- @endpointservices/footer-with-backups/6eda90668ae03044@830.js
+++ @endpointservices/footer-with-backups/6eda90668ae03044@830.js
@@ -1,6 +1,6 @@
 // https://observablehq.com/@endpointservices/webcode@830
-import define1 from "./cbc2e7f6260d11b1@2758.js";
-import define2 from "./dff1e917c89f5e76@1886.js";
+import define1 from "./cbc2e7f6260d11b1@2761.js";
+import define2 from "./dff1e917c89f5e76@1939.js";
 import define3 from "./58f3eb7334551ae6@209.js";
 
 function _1(html){return(
