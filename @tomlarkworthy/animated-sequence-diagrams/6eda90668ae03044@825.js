0df9d9686b8a213bd079dad9f15543162fa7e4cd
backup-to-github <robot@webcode.run>
2022-06-17 05:21:06 +0000

    Backup https://observablehq.com/@tomlarkworthy/animated-sequence-diagrams   url:     https://observablehq.com/@tomlarkworthy/animated-sequence-diagrams   title:   Interactive Sequence Diagrams   author:  Tom Larkworthy (@tomlarkworthy)   id:      4c8138da42ecfcec   version: 186

--- @tomlarkworthy/animated-sequence-diagrams/6eda90668ae03044@825.js
+++ @tomlarkworthy/animated-sequence-diagrams/6eda90668ae03044@825.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@endpointservices/webcode@825
-import define1 from "./cbc2e7f6260d11b1@2728.js";
+import define1 from "./cbc2e7f6260d11b1@2761.js";
 import define2 from "./dff1e917c89f5e76@1886.js";
 import define3 from "./58f3eb7334551ae6@209.js";
 
