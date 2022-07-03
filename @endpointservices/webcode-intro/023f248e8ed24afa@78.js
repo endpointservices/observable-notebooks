63cef8ae5ffb1498044328531429e5bacb2a558b
backup-to-github <robot@webcode.run>
2022-07-03 09:39:47 +0000

    Backup https://observablehq.com/@endpointservices/webcode-intro   url:     https://observablehq.com/@endpointservices/webcode-intro   title:   My First Webcode Endpoint   author:  Endpoint Services (@endpointservices)   id:      023f248e8ed24afa   version: 78

--- @endpointservices/webcode-intro/023f248e8ed24afa@78.js
+++ @endpointservices/webcode-intro/023f248e8ed24afa@78.js
@@ -1,5 +1,5 @@
-import define1 from "./6eda90668ae03044@804.js";
-import define2 from "./293899bef371e135@247.js";
+import define1 from "./6eda90668ae03044@830.js";
+import define2 from "./293899bef371e135@278.js";
 
 function _1(md){return(
 md`# My First Webcode Endpoint`
