f3b48a97db909560ba9e02a520de02bb59ce2aa0
backup-to-github <robot@webcode.run>
2022-07-18 11:29:34 +0000

    Backup https://observablehq.com/@endpointservices/how-to-arrow-client   url:     https://observablehq.com/@endpointservices/how-to-arrow-client   title:   Serverless arrow file consumption   author:  Endpoint Services (@endpointservices)   id:      552a5ae0888014fd   version: 104

--- @endpointservices/how-to-arrow-client/552a5ae0888014fd@104.js
+++ @endpointservices/how-to-arrow-client/552a5ae0888014fd@104.js
@@ -1,6 +1,6 @@
 // https://observablehq.com/@endpointservices/how-to-arrow-client@104
 import define1 from "./a2e58f97fd5e8d7c@736.js";
-import define2 from "./293899bef371e135@271.js";
+import define2 from "./293899bef371e135@278.js";
 
 async function _1(md,FileAttachment){return(
 md`![](${await FileAttachment("ApachearrowClient@1.png").url()})
