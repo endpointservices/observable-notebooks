cceb5e26a9f4e529b35c9a2380a04d089fac2c1e
backup-to-github <robot@webcode.run>
2022-09-19 09:07:41 +0000

    Backup https://observablehq.com/@endpointservices/deploy-and-test-latency   url:     https://observablehq.com/@endpointservices/deploy-and-test-latency   title:   Deploy and Test webcode workflow latency measure   author:  Endpoint Services (@endpointservices)   id:      13a525f8b7fff694   version: 180

--- @endpointservices/deploy-and-test-latency/13a525f8b7fff694@180.js
+++ @endpointservices/deploy-and-test-latency/13a525f8b7fff694@180.js
@@ -1,5 +1,5 @@
 import define1 from "./6eda90668ae03044@836.js";
-import define2 from "./293899bef371e135@280.js";
+import define2 from "./293899bef371e135@290.js";
 
 function _1(md){return(
 md`# Deploy and Test webcode workflow latency measure
