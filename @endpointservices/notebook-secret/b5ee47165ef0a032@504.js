54cacbfb52c6aeaa28d67f6e86f1aeb0c4359e6e
backup-to-github <robot@webcode.run>
2022-09-19 09:07:02 +0000

    Backup https://observablehq.com/@endpointservices/notebook-secret   url:     https://observablehq.com/@endpointservices/notebook-secret   title:   How to password protect a Notebook secret   author:  Endpoint Services (@endpointservices)   id:      b5ee47165ef0a032   version: 504

--- @endpointservices/notebook-secret/b5ee47165ef0a032@504.js
+++ @endpointservices/notebook-secret/b5ee47165ef0a032@504.js
@@ -1,8 +1,8 @@
 // https://observablehq.com/@endpointservices/notebook-secret@504
 import define1 from "./11a5ab8b1b3a51db@1161.js";
-import define2 from "./a2e58f97fd5e8d7c@674.js";
+import define2 from "./a2e58f97fd5e8d7c@736.js";
 import define3 from "./ab3e70b29c480e6d@83.js";
-import define4 from "./293899bef371e135@225.js";
+import define4 from "./293899bef371e135@290.js";
 
 function _1(md){return(
 md`# How to password protect a Notebook secret
