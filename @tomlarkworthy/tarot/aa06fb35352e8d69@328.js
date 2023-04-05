33edefecf7814463fcb7fbaa08c141c5e9c105e7
backup-to-github <robot@webcode.run>
2023-04-05 07:35:13 +0000

    Backup https://observablehq.com/@tomlarkworthy/tarot   url:     https://observablehq.com/@tomlarkworthy/tarot   title:   Tarot   author:  Tom Larkworthy (@tomlarkworthy)   id:      aa06fb35352e8d69   version: 328

--- @tomlarkworthy/tarot/aa06fb35352e8d69@328.js
+++ @tomlarkworthy/tarot/aa06fb35352e8d69@328.js
@@ -1,4 +1,4 @@
-import define1 from "./549f62919a7018eb@4378.js";
+import define1 from "./549f62919a7018eb@4483.js";
 import define2 from "./293899bef371e135@290.js";
 
 function _app(transitions,$0){return(
