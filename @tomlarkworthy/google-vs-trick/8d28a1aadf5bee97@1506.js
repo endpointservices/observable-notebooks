6670e8348bf0ce65addab2e2dc2216bb9c45435d
backup-to-github <robot@webcode.run>
2023-12-30 14:39:53 +0000

    Backup https://observablehq.com/@tomlarkworthy/google-vs-trick   url:     https://observablehq.com/@tomlarkworthy/google-vs-trick   title:   The Google VS trick   author:  Tom Larkworthy (@tomlarkworthy)   id:      8d28a1aadf5bee97   version: 1506

--- @tomlarkworthy/google-vs-trick/8d28a1aadf5bee97@1506.js
+++ @tomlarkworthy/google-vs-trick/8d28a1aadf5bee97@1506.js
@@ -2,12 +2,12 @@
 import define1 from "./b5172ba044a84c1e@665.js";
 import define2 from "./18498b485712f206@114.js";
 import define3 from "./0e0b35a92c819d94@444.js";
-import define4 from "./048a17a165be198d@263.js";
+import define4 from "./048a17a165be198d@264.js";
 import define5 from "./3d321b3f7d398726@462.js";
 import define6 from "./027541187c96745d@147.js";
 import define7 from "./f92778131fd76559@1174.js";
 import define8 from "./bb2055d580bbbab2@106.js";
-import define9 from "./293899bef371e135@290.js";
+import define9 from "./293899bef371e135@293.js";
 
 function _1(md){return(
 md`# The Google 'VS' trick
