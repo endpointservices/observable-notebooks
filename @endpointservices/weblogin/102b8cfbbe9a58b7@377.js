12a5c91380133ec69b5f30b654eb8d56fad4f571
backup-to-github <robot@webcode.run>
2024-02-04 05:18:05 +0000

    Backup https://observablehq.com/@endpointservices/weblogin   url:     https://observablehq.com/@endpointservices/weblogin   title:   User signin with IndieWeb weblogin   author:  Endpoint Services (@endpointservices)   id:      102b8cfbbe9a58b7   version: 377

--- @endpointservices/weblogin/102b8cfbbe9a58b7@377.js
+++ @endpointservices/weblogin/102b8cfbbe9a58b7@377.js
@@ -2,7 +2,7 @@ import define1 from "./7a77afd2ce564067@459.js";
 import define2 from "./c2dae147641e012a@46.js";
 import define3 from "./ef672b935bd480fc@623.js";
 import define4 from "@observablehq/banner@463";
-import define5 from "./293899bef371e135@290.js";
+import define5 from "./293899bef371e135@293.js";
 
 async function _1(bannerImage,FileAttachment){return(
 bannerImage(await FileAttachment("Weblogin.png").url(), "User signin with IndieWeb weblogin")
