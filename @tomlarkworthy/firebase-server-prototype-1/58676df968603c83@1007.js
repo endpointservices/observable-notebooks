68c321dc8470e719898cfd327140497a2896c69c
backup-to-github <robot@webcode.run>
2024-10-04 23:21:07 +0000

    Backup https://observablehq.com/@tomlarkworthy/firebase-server-prototype-1   url:     https://observablehq.com/@tomlarkworthy/firebase-server-prototype-1   title:   author:   id:

--- @tomlarkworthy/firebase-server-prototype-1/58676df968603c83@1007.js
+++ @tomlarkworthy/firebase-server-prototype-1/58676df968603c83@1007.js
@@ -4,7 +4,7 @@ import define2 from "./6a703b03d185f279@1005.js";
 import define3 from "./374124b361974cb3@265.js";
 import define4 from "./5a63548f9c799b76@576.js";
 import define5 from "./6eda90668ae03044@836.js";
-import define6 from "./c7a3b20cec5d4dd9@669.js";
+import define6 from "./c7a3b20cec5d4dd9@725.js";
 import define7 from "./0e0b35a92c819d94@444.js";
 import define8 from "./293899bef371e135@293.js";
 import define9 from "./64641700df65baed@91.js";
