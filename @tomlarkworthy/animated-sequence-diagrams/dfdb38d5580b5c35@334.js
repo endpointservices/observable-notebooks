69c0529a908fa0a2da389e882cd95063f0a6a092
backup-to-github <robot@webcode.run>
2024-10-04 23:17:04 +0000

    Backup https://observablehq.com/@tomlarkworthy/animated-sequence-diagrams   url:     https://observablehq.com/@tomlarkworthy/animated-sequence-diagrams   title:   author:   id:

--- @tomlarkworthy/animated-sequence-diagrams/dfdb38d5580b5c35@334.js
+++ @tomlarkworthy/animated-sequence-diagrams/dfdb38d5580b5c35@334.js
@@ -9,15 +9,22 @@ import {footer} from "@tomlarkworthy/footer"
 `
 )}
 
-function _3(footer){return(
+function _2($0){return(
+$0
+)}
+
+function _4(footer){return(
 footer
 )}
 
 export default function define(runtime, observer) {
   const main = runtime.module();
   main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["viewof backups"], _2);
   const child1 = runtime.module(define1);
   main.import("footer", child1);
-  main.variable(observer()).define(["footer"], _3);
+  main.import("viewof backups", child1);
+  main.import("backups", child1);
+  main.variable(observer()).define(["footer"], _4);
   return main;
 }
