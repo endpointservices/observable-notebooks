c928203431ad550d95bcebdbc36d833cd00e4da3
backup-to-github <robot@webcode.run>
2024-11-19 21:53:16 +0000

    Backup https://observablehq.com/@tomlarkworthy/scrape-notebook-history   url:     https://observablehq.com/@tomlarkworthy/scrape-notebook-history   title:   author:   id:

---
+++ @tomlarkworthy/scrape-notebook-history/869e23521ea3e7eb@2912.js
@@ -0,0 +1,624 @@
+import define1 from "./17c8ce433e1df58e@3332.js";
+import define2 from "./e93eab08140b49b2@2875.js";
+import define3 from "./ef672b935bd480fc@623.js";
+import define4 from "./048a17a165be198d@271.js";
+
+function _1(md){return(
+md`# Notebook Process Visualizer
+
+Notes: only fetching by cookie works
+`
+)}
+
+function _example(){return(
+{
+  first_public_version: null,
+  paused_version: null,
+  published_versions: [],
+  events: [
+    {
+      type: "create",
+      version: 2763,
+      node_id: null,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:43:49.741Z"
+    },
+    {
+      type: "modify_node",
+      version: 2765,
+      node_id: 2764,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:46:00.349Z",
+      is_new: true,
+      node_value_start: 'Inputs.input("notebook URL")'
+    },
+    {
+      type: "unpin_node",
+      version: 2766,
+      node_id: 2764,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:46:02.601Z"
+    },
+    {
+      type: "modify_node",
+      version: 2767,
+      node_id: 2764,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:46:18.167Z",
+      node_name: "notebookURL"
+    },
+    {
+      type: "remove_node",
+      version: 2768,
+      node_id: 2764,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:46:29.687Z"
+    },
+    {
+      type: "modify_node",
+      version: 2770,
+      node_id: 2769,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:46:56.598Z",
+      is_new: true,
+      node_name: "targetURL"
+    },
+    {
+      type: "unpin_node",
+      version: 2771,
+      node_id: 2769,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:47:01.299Z"
+    },
+    {
+      type: "modify_node",
+      version: 2773,
+      node_id: 2772,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:47:48.714Z",
+      is_new: true,
+      node_value_start: 'import { fetchp } from "@tomlarkworthy/'
+    },
+    {
+      type: "unpin_node",
+      version: 2774,
+      node_id: 2772,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-05T05:47:50.343Z"
+    },
+    {
+      type: "modify_node",
+      version: 2775,
+      node_id: 0,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T07:57:25.571Z",
+      node_value_start: "# Notebook Process Visualizer\n"
+    },
+    {
+      type: "modify_title",
+      version: 2776,
+      node_id: null,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T07:57:25.717Z",
+      title: "Notebook Process Visualizer"
+    },
+    {
+      type: "modify_node",
+      version: 2778,
+      node_id: 2777,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:13:34.186Z",
+      is_new: true,
+      node_name: "jwtPassword"
+    },
+    {
+      type: "unpin_node",
+      version: 2779,
+      node_id: 2777,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:13:35.477Z"
+    },
+    {
+      type: "modify_node",
+      version: 2780,
+      node_id: 2777,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:13:49.334Z",
+      node_name: "jwtPassword"
+    },
+    {
+      type: "remove_node",
+      version: 2781,
+      node_id: 2777,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:17:10.977Z"
+    },
+    {
+      type: "remove_node",
+      version: 2782,
+      node_id: 2769,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:17:15.954Z"
+    },
+    {
+      type: "modify_node",
+      version: 2784,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:17:54.288Z",
+      is_new: true,
+      node_name: "notebook_json"
+    },
+    {
+      type: "modify_node",
+      version: 2785,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:17:59.443Z",
+      node_name: "notebook_json"
+    },
+    {
+      type: "modify_node",
+      version: 2787,
+      node_id: 2786,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:18:18.577Z",
+      is_new: true,
+      node_value_start: "highlight(notebook_json)"
+    },
+    {
+      type: "unpin_node",
+      version: 2788,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:18:58.908Z"
+    },
+    {
+      type: "modify_node",
+      version: 2789,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:19:08.318Z",
+      node_name: "notebook_json"
+    },
+    {
+      type: "modify_node",
+      version: 2790,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:19:32.257Z",
+      node_name: "notebook_json"
+    },
+    {
+      type: "modify_node",
+      version: 2792,
+      node_id: 2791,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:20:43.143Z",
+      is_new: true,
+      node_name: "events_over_time"
+    },
+    {
+      type: "remove_node",
+      version: 2793,
+      node_id: 2791,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:20:48.080Z"
+    },
+    {
+      type: "modify_node",
+      version: 2795,
+      node_id: 2794,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:21:46.713Z",
+      is_new: true,
+      node_value_start: "Plot.plot({\n    y: {\n        label: 'Ac"
+    },
+    {
+      type: "modify_node",
+      version: 2796,
+      node_id: 2794,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:21:49.636Z",
+      node_value_start: 'Plot.plot({\n  y: {\n    label: "Action",'
+    },
+    {
+      type: "modify_node",
+      version: 2798,
+      node_id: 2797,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:22:20.623Z",
+      is_new: true,
+      node_name: "history"
+    },
+    {
+      type: "modify_node",
+      version: 2799,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:22:25.085Z",
+      node_name: "notebook_history_str"
+    },
+    {
+      type: "modify_node",
+      version: 2800,
+      node_id: 2786,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:22:28.489Z",
+      node_value_start: "highlight(notebook_history_str)"
+    },
+    {
+      type: "modify_node",
+      version: 2801,
+      node_id: 2786,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:22:35.401Z",
+      node_value_start: "highlight(history)"
+    },
+    {
+      type: "modify_node",
+      version: 2802,
+      node_id: 2783,
+      user_id: "5215f6ec4a999d40",
+      time: "2024-10-06T09:24:02.311Z",
+      node_name: "notebook_history_str"
+    }
+  ],
+  merges: [],
+  users: [
+    {
+      id: "5215f6ec4a999d40",
+      avatar_url:
+        "https://avatars.observableusercontent.com/avatar/47327a8bc1966f2186dcb3ebf4b7ee6e4e7ab9a5c2a07405aff57200ea778f71",
+      login: "tomlarkworthy",
+      name: "Tom Larkworthy",
+      bio: "Tech Lead at Taktile.\nFormerly Firebase, Google",
+      home_url: "https://taktile.com",
+      tier: "pro"
+    }
+  ],
+  identifiers: [
+    [
+      0,
+      {
+        node_value_start: "# Roboco-op: Blank Slate <!-- REWRITE TH"
+      }
+    ],
+    [
+      81,
+      {
+        node_value_start: "viewof prompt"
+      }
+    ],
+    [
+      1014,
+      {
+        node_value_start: 'Inputs.button("copy code", {\n  reduce: ('
+      }
+    ],
+    [
+      105,
+      {
+        node_value_start: "viewof suggestion"
+      }
+    ],
+    [
+      1463,
+      {
+        node_value_start: "## Current Chat context\ncode is automati"
+      }
+    ],
+    [
+      1252,
+      {
+        node_value_start: "viewof context_viz"
+      }
+    ],
+    [
+      1692,
+      {
+        node_value_start: "### AI Settings"
+      }
+    ],
+    [
+      29,
+      {
+        node_value_start: "viewof OPENAI_API_KEY"
+      }
+    ],
+    [
+      2061,
+      {
+        node_value_start: "viewof api_endpoint"
+      }
+    ],
+    [
+      2163,
+      {
+        node_value_start: "viewof settings"
+      }
+    ],
+    [
+      2760,
+      {
+        node_value_start: "rag"
+      }
+    ],
+    [
+      2193,
+      {
+        node_value_start: "---"
+      }
+    ],
+    [
+      2114,
+      {
+        node_value_start: "import {\n  ask,\n  excludes,\n  cells,\n  u"
+      }
+    ],
+    [
+      2738,
+      {
+        node_value_start: 'import { extension as rag } from "@tomla'
+      }
+    ],
+    [
+      2179,
+      {
+        node_value_start: "background_tasks"
+      }
+    ]
+  ]
+}
+)}
+
+function _observable_api_key(Inputs,localStorageView){return(
+Inputs.bind(
+  Inputs.password({
+    label: "Observable API KEY"
+  }),
+  localStorageView("OBSERVABLE_API_KEY")
+)
+)}
+
+function _notebook_history_str(Inputs,example){return(
+{
+  prompt:
+    "Add a text area for pasting the nobook /history endpoint response JSON",
+  time: 1728206267469
+} &&
+  Inputs.textarea({
+    label: "Paste notebookc/history endpoint response JSON here",
+    placeholder: "{...}",
+    value: JSON.stringify(example, null, 2)
+  })
+)}
+
+function _history(notebook_history_str){return(
+JSON.parse(notebook_history_str)
+)}
+
+function _events(history){return(
+history.events.map((e) => ({
+  ...e,
+  time: new Date(e.time)
+}))
+)}
+
+function _7(highlight,events){return(
+highlight(events)
+)}
+
+function _sample_version(events){return(
+events[0].version
+)}
+
+async function _9(fetchp){return(
+(await fetchp(`https://api.observablehq.com/document/869e23521ea3e7eb`)).text()
+)}
+
+async function _10(fetchp){return(
+(
+  await fetchp(`https://api.observablehq.com/document/869e23521ea3e7eb@2846`)
+).text()
+)}
+
+async function _11(fetchp){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/document/@tomlarkworthy/scrape-notebook-history`
+  )
+).text()
+)}
+
+async function _12(fetchp){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/document/@tomlarkworthy/scrape-notebook-history@2846`
+  )
+).text()
+)}
+
+async function _13(fetchp){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/document/@tomlarkworthy/scrape-notebook-history@2846`
+  )
+).text()
+)}
+
+async function _14(fetchp,observable_api_key){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/d/869e23521ea3e7eb@2844.js?v=3&api_key=${observable_api_key}`,
+    {
+      headers: {
+        authorization: `APIKey ${observable_api_key}`
+      }
+    }
+  )
+).text()
+)}
+
+async function _15(fetchp,observable_api_key){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/d/869e23521ea3e7eb@2844.js?v=3&api_key=${observable_api_key}`,
+    {
+      headers: {
+        origin: "https://observablehq.com",
+        authorization: `APIKey ${observable_api_key}`
+      }
+    }
+  )
+).text()
+)}
+
+async function _16(fetchp,observable_api_key){return(
+(
+  await fetchp(
+    `https://api.observablehq.com/document/@tomlarkworthy/scrape-notebook-history@2768`,
+    {
+      headers: {
+        Authorization: `APIKey ${observable_api_key}`
+      }
+    }
+  )
+).text()
+)}
+
+async function _17(fetchp){return(
+(
+  await fetchp(
+    `https://webcode.run/observablehq.com/@endpointservices/realtime-request-log`,
+    {
+      headers: {
+        authorization: `Bearer eyJpZCI6IjUyMTVmNmVjNGE5OTlkNDAiLCJleHBpcmF0aW9uIjoxNzI4NzU2MDAwMDAwLCJpZHMiOlt7ImlkIjoiNTUxNWU1MzJlOTYxNmFkMiIsIm9yZyI6IiIsInNvIjoxNzIxNDE2NzY3NzExLCJhdCI6MTcyODIyMTQwMDAwMH1dLCJhbHBoYSI6dHJ1ZSwiYmV0YSI6ZmFsc2V9.M1VYZdhwWP7y2M7PdMHBfOiGnOJWKFlnqpSFLb2Dghg=`
+      }
+    }
+  )
+).text()
+)}
+
+function _18($0){return(
+$0
+)}
+
+function _19(Inputs,suggestion){return(
+Inputs.button("copy code", {
+  reduce: () => {
+    navigator.clipboard.writeText(suggestion);
+  }
+})
+)}
+
+function _20($0){return(
+$0
+)}
+
+function _21(md){return(
+md`## Current Chat context
+code is automatically added to the context. Use \`highlight(<expr>)\` to selectively bring runtime values into the context as well`
+)}
+
+function _22($0){return(
+$0
+)}
+
+function _23(md){return(
+md`### AI Settings`
+)}
+
+function _24($0){return(
+$0
+)}
+
+function _25($0){return(
+$0
+)}
+
+function _26($0){return(
+$0
+)}
+
+function _27(rag){return(
+rag
+)}
+
+function _28(md){return(
+md`---`
+)}
+
+function _33(background_tasks){return(
+background_tasks
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("example")).define("example", _example);
+  main.variable(observer("viewof observable_api_key")).define("viewof observable_api_key", ["Inputs","localStorageView"], _observable_api_key);
+  main.variable(observer("observable_api_key")).define("observable_api_key", ["Generators", "viewof observable_api_key"], (G, _) => G.input(_));
+  main.variable(observer("viewof notebook_history_str")).define("viewof notebook_history_str", ["Inputs","example"], _notebook_history_str);
+  main.variable(observer("notebook_history_str")).define("notebook_history_str", ["Generators", "viewof notebook_history_str"], (G, _) => G.input(_));
+  main.variable(observer("history")).define("history", ["notebook_history_str"], _history);
+  main.variable(observer("events")).define("events", ["history"], _events);
+  main.variable(observer()).define(["highlight","events"], _7);
+  main.variable(observer("sample_version")).define("sample_version", ["events"], _sample_version);
+  main.variable(observer()).define(["fetchp"], _9);
+  main.variable(observer()).define(["fetchp"], _10);
+  main.variable(observer()).define(["fetchp"], _11);
+  main.variable(observer()).define(["fetchp"], _12);
+  main.variable(observer()).define(["fetchp"], _13);
+  main.variable(observer()).define(["fetchp","observable_api_key"], _14);
+  main.variable(observer()).define(["fetchp","observable_api_key"], _15);
+  main.variable(observer()).define(["fetchp","observable_api_key"], _16);
+  main.variable(observer()).define(["fetchp"], _17);
+  main.variable(observer()).define(["viewof prompt"], _18);
+  main.variable(observer()).define(["Inputs","suggestion"], _19);
+  main.variable(observer()).define(["viewof suggestion"], _20);
+  main.variable(observer()).define(["md"], _21);
+  main.variable(observer()).define(["viewof context_viz"], _22);
+  main.variable(observer()).define(["md"], _23);
+  main.variable(observer()).define(["viewof OPENAI_API_KEY"], _24);
+  main.variable(observer()).define(["viewof api_endpoint"], _25);
+  main.variable(observer()).define(["viewof settings"], _26);
+  main.variable(observer()).define(["rag"], _27);
+  main.variable(observer()).define(["md"], _28);
+  const child1 = runtime.module(define1);
+  main.import("ask", child1);
+  main.import("excludes", child1);
+  main.import("cells", child1);
+  main.import("update_context", child1);
+  main.import("on_prompt", child1);
+  main.import("variables", child1);
+  main.import("api_call_response", child1);
+  main.import("background_tasks", child1);
+  main.import("ndd", child1);
+  main.import("_ndd", child1);
+  main.import("instruction", child1);
+  main.import("_events", child1);
+  main.import("highlight", child1);
+  main.import("mutable context", child1);
+  main.import("context", child1);
+  main.import("viewof prompt", child1);
+  main.import("prompt", child1);
+  main.import("viewof suggestion", child1);
+  main.import("suggestion", child1);
+  main.import("viewof settings", child1);
+  main.import("settings", child1);
+  main.import("viewof OPENAI_API_KEY", child1);
+  main.import("OPENAI_API_KEY", child1);
+  main.import("viewof api_endpoint", child1);
+  main.import("api_endpoint", child1);
+  main.import("viewof context_viz", child1);
+  main.import("context_viz", child1);
+  const child2 = runtime.module(define2);
+  main.import("extension", "rag", child2);
+  const child3 = runtime.module(define3);
+  main.import("fetchp", child3);
+  const child4 = runtime.module(define4);
+  main.import("localStorageView", child4);
+  main.variable(observer()).define(["background_tasks"], _33);
+  return main;
+}
---
+++ @tomlarkworthy/scrape-notebook-history/index.js
@@ -0,0 +1,1 @@
+export {default} from "./869e23521ea3e7eb@2912.js";
