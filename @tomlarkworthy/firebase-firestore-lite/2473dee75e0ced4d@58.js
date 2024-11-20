d60258ffc7ad0bb9467a8ae54ef170b019c5421a
backup-to-github <robot@webcode.run>
2024-11-20 23:37:44 +0000

    Backup https://observablehq.com/@tomlarkworthy/firebase-firestore-lite   url:     https://observablehq.com/@tomlarkworthy/firebase-firestore-lite   title:   author:   id:

---
+++ @tomlarkworthy/firebase-firestore-lite/2473dee75e0ced4d@58.js
@@ -0,0 +1,264 @@
+function _1(md){return(
+md`# Firebase Firestore Lite`
+)}
+
+function _https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js(){return(
+import('https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore-lite.js')
+)}
+
+function _Bytes(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['Bytes']
+)}
+
+function _CollectionReference(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['CollectionReference']
+)}
+
+function _DocumentReference(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['DocumentReference']
+)}
+
+function _DocumentSnapshot(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['DocumentSnapshot']
+)}
+
+function _FieldPath(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['FieldPath']
+)}
+
+function _FieldValue(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['FieldValue']
+)}
+
+function _Firestore(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['Firestore']
+)}
+
+function _FirestoreError(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['FirestoreError']
+)}
+
+function _GeoPoint(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['GeoPoint']
+)}
+
+function _Query(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['Query']
+)}
+
+function _QueryConstraint(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['QueryConstraint']
+)}
+
+function _QueryDocumentSnapshot(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['QueryDocumentSnapshot']
+)}
+
+function _QuerySnapshot(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['QuerySnapshot']
+)}
+
+function _Timestamp(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['Timestamp']
+)}
+
+function _Transaction(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['Transaction']
+)}
+
+function _WriteBatch(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['WriteBatch']
+)}
+
+function _addDoc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['addDoc']
+)}
+
+function _arrayRemove(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['arrayRemove']
+)}
+
+function _arrayUnion(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['arrayUnion']
+)}
+
+function _collection(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['collection']
+)}
+
+function _collectionGroup(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['collectionGroup']
+)}
+
+function _connectFirestoreEmulator(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['connectFirestoreEmulator']
+)}
+
+function _deleteDoc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['deleteDoc']
+)}
+
+function _deleteField(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['deleteField']
+)}
+
+function _doc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['doc']
+)}
+
+function _documentId(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['documentId']
+)}
+
+function _endAt(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['endAt']
+)}
+
+function _endBefore(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['endBefore']
+)}
+
+function _getDoc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['getDoc']
+)}
+
+function _getDocs(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['getDocs']
+)}
+
+function _getFirestore(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['getFirestore']
+)}
+
+function _increment(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['increment']
+)}
+
+function _initializeFirestore(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['initializeFirestore']
+)}
+
+function _limit(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['limit']
+)}
+
+function _limitToLast(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['limitToLast']
+)}
+
+function _orderBy(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['orderBy']
+)}
+
+function _query(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['query']
+)}
+
+function _queryEqual(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['queryEqual']
+)}
+
+function _refEqual(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['refEqual']
+)}
+
+function _runTransaction(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['runTransaction']
+)}
+
+function _serverTimestamp(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['serverTimestamp']
+)}
+
+function _setDoc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['setDoc']
+)}
+
+function _setLogLevel(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['setLogLevel']
+)}
+
+function _snapshotEqual(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['snapshotEqual']
+)}
+
+function _startAfter(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['startAfter']
+)}
+
+function _startAt(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['startAt']
+)}
+
+function _terminate(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['terminate']
+)}
+
+function _updateDoc(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['updateDoc']
+)}
+
+function _where(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['where']
+)}
+
+function _writeBatch(https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js['writeBatch']
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js")).define("https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js", _https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js);
+  main.variable(observer("Bytes")).define("Bytes", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _Bytes);
+  main.variable(observer("CollectionReference")).define("CollectionReference", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _CollectionReference);
+  main.variable(observer("DocumentReference")).define("DocumentReference", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _DocumentReference);
+  main.variable(observer("DocumentSnapshot")).define("DocumentSnapshot", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _DocumentSnapshot);
+  main.variable(observer("FieldPath")).define("FieldPath", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _FieldPath);
+  main.variable(observer("FieldValue")).define("FieldValue", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _FieldValue);
+  main.variable(observer("Firestore")).define("Firestore", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _Firestore);
+  main.variable(observer("FirestoreError")).define("FirestoreError", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _FirestoreError);
+  main.variable(observer("GeoPoint")).define("GeoPoint", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _GeoPoint);
+  main.variable(observer("Query")).define("Query", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _Query);
+  main.variable(observer("QueryConstraint")).define("QueryConstraint", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _QueryConstraint);
+  main.variable(observer("QueryDocumentSnapshot")).define("QueryDocumentSnapshot", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _QueryDocumentSnapshot);
+  main.variable(observer("QuerySnapshot")).define("QuerySnapshot", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _QuerySnapshot);
+  main.variable(observer("Timestamp")).define("Timestamp", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _Timestamp);
+  main.variable(observer("Transaction")).define("Transaction", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _Transaction);
+  main.variable(observer("WriteBatch")).define("WriteBatch", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _WriteBatch);
+  main.variable(observer("addDoc")).define("addDoc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _addDoc);
+  main.variable(observer("arrayRemove")).define("arrayRemove", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _arrayRemove);
+  main.variable(observer("arrayUnion")).define("arrayUnion", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _arrayUnion);
+  main.variable(observer("collection")).define("collection", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _collection);
+  main.variable(observer("collectionGroup")).define("collectionGroup", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _collectionGroup);
+  main.variable(observer("connectFirestoreEmulator")).define("connectFirestoreEmulator", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _connectFirestoreEmulator);
+  main.variable(observer("deleteDoc")).define("deleteDoc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _deleteDoc);
+  main.variable(observer("deleteField")).define("deleteField", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _deleteField);
+  main.variable(observer("doc")).define("doc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _doc);
+  main.variable(observer("documentId")).define("documentId", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _documentId);
+  main.variable(observer("endAt")).define("endAt", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _endAt);
+  main.variable(observer("endBefore")).define("endBefore", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _endBefore);
+  main.variable(observer("getDoc")).define("getDoc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _getDoc);
+  main.variable(observer("getDocs")).define("getDocs", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _getDocs);
+  main.variable(observer("getFirestore")).define("getFirestore", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _getFirestore);
+  main.variable(observer("increment")).define("increment", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _increment);
+  main.variable(observer("initializeFirestore")).define("initializeFirestore", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _initializeFirestore);
+  main.variable(observer("limit")).define("limit", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _limit);
+  main.variable(observer("limitToLast")).define("limitToLast", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _limitToLast);
+  main.variable(observer("orderBy")).define("orderBy", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _orderBy);
+  main.variable(observer("query")).define("query", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _query);
+  main.variable(observer("queryEqual")).define("queryEqual", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _queryEqual);
+  main.variable(observer("refEqual")).define("refEqual", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _refEqual);
+  main.variable(observer("runTransaction")).define("runTransaction", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _runTransaction);
+  main.variable(observer("serverTimestamp")).define("serverTimestamp", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _serverTimestamp);
+  main.variable(observer("setDoc")).define("setDoc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _setDoc);
+  main.variable(observer("setLogLevel")).define("setLogLevel", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _setLogLevel);
+  main.variable(observer("snapshotEqual")).define("snapshotEqual", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _snapshotEqual);
+  main.variable(observer("startAfter")).define("startAfter", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _startAfter);
+  main.variable(observer("startAt")).define("startAt", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _startAt);
+  main.variable(observer("terminate")).define("terminate", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _terminate);
+  main.variable(observer("updateDoc")).define("updateDoc", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _updateDoc);
+  main.variable(observer("where")).define("where", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _where);
+  main.variable(observer("writeBatch")).define("writeBatch", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_firestore_lite_js"], _writeBatch);
+  return main;
+}
---
+++ @tomlarkworthy/firebase-firestore-lite/index.js
@@ -0,0 +1,1 @@
+export {default} from "./2473dee75e0ced4d@58.js";
