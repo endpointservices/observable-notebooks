c87aec0c264592029fb47984260d55a7f23949f3
backup-to-github <robot@webcode.run>
2024-11-20 23:41:49 +0000

    Backup https://observablehq.com/@tomlarkworthy/firebase-storage   url:     https://observablehq.com/@tomlarkworthy/firebase-storage   title:   author:   id:

---
+++ @tomlarkworthy/firebase-storage/2a16cfdefe40fe1d@34.js
@@ -0,0 +1,139 @@
+function _1(md){return(
+md`# Firebase Storage`
+)}
+
+function _https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js(){return(
+import('https://www.gstatic.com/firebasejs/9.9.2/firebase-storage.js')
+)}
+
+function _StringFormat(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['StringFormat']
+)}
+
+function __FbsBlob(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_FbsBlob']
+)}
+
+function __Location(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_Location']
+)}
+
+function __TaskEvent(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_TaskEvent']
+)}
+
+function __TaskState(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_TaskState']
+)}
+
+function __UploadTask(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_UploadTask']
+)}
+
+function __dataFromString(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_dataFromString']
+)}
+
+function __getChild(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_getChild']
+)}
+
+function __invalidArgument(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_invalidArgument']
+)}
+
+function __invalidRootOperation(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['_invalidRootOperation']
+)}
+
+function _connectStorageEmulator(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['connectStorageEmulator']
+)}
+
+function _deleteObject(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['deleteObject']
+)}
+
+function _getBlob(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getBlob']
+)}
+
+function _getBytes(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getBytes']
+)}
+
+function _getDownloadURL(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getDownloadURL']
+)}
+
+function _getMetadata(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getMetadata']
+)}
+
+function _getStorage(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getStorage']
+)}
+
+function _getStream(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['getStream']
+)}
+
+function _list(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['list']
+)}
+
+function _listAll(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['listAll']
+)}
+
+function _ref(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['ref']
+)}
+
+function _updateMetadata(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['updateMetadata']
+)}
+
+function _uploadBytes(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['uploadBytes']
+)}
+
+function _uploadBytesResumable(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['uploadBytesResumable']
+)}
+
+function _uploadString(https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js){return(
+https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js['uploadString']
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer("https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js")).define("https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js", _https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js);
+  main.variable(observer("StringFormat")).define("StringFormat", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _StringFormat);
+  main.variable(observer("_FbsBlob")).define("_FbsBlob", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __FbsBlob);
+  main.variable(observer("_Location")).define("_Location", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __Location);
+  main.variable(observer("_TaskEvent")).define("_TaskEvent", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __TaskEvent);
+  main.variable(observer("_TaskState")).define("_TaskState", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __TaskState);
+  main.variable(observer("_UploadTask")).define("_UploadTask", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __UploadTask);
+  main.variable(observer("_dataFromString")).define("_dataFromString", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __dataFromString);
+  main.variable(observer("_getChild")).define("_getChild", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __getChild);
+  main.variable(observer("_invalidArgument")).define("_invalidArgument", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __invalidArgument);
+  main.variable(observer("_invalidRootOperation")).define("_invalidRootOperation", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], __invalidRootOperation);
+  main.variable(observer("connectStorageEmulator")).define("connectStorageEmulator", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _connectStorageEmulator);
+  main.variable(observer("deleteObject")).define("deleteObject", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _deleteObject);
+  main.variable(observer("getBlob")).define("getBlob", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getBlob);
+  main.variable(observer("getBytes")).define("getBytes", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getBytes);
+  main.variable(observer("getDownloadURL")).define("getDownloadURL", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getDownloadURL);
+  main.variable(observer("getMetadata")).define("getMetadata", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getMetadata);
+  main.variable(observer("getStorage")).define("getStorage", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getStorage);
+  main.variable(observer("getStream")).define("getStream", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _getStream);
+  main.variable(observer("list")).define("list", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _list);
+  main.variable(observer("listAll")).define("listAll", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _listAll);
+  main.variable(observer("ref")).define("ref", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _ref);
+  main.variable(observer("updateMetadata")).define("updateMetadata", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _updateMetadata);
+  main.variable(observer("uploadBytes")).define("uploadBytes", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _uploadBytes);
+  main.variable(observer("uploadBytesResumable")).define("uploadBytesResumable", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _uploadBytesResumable);
+  main.variable(observer("uploadString")).define("uploadString", ["https___www_gstatic_com_firebasejs_9_9_2_firebase_storage_js"], _uploadString);
+  return main;
+}
---
+++ @tomlarkworthy/firebase-storage/index.js
@@ -0,0 +1,1 @@
+export {default} from "./2a16cfdefe40fe1d@34.js";
