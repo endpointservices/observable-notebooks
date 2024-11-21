107064b37267e605ba496452828e4184c4cabfa8
backup-to-github <robot@webcode.run>
2024-11-21 01:24:33 +0000

    Backup https://observablehq.com/@tomlarkworthy/testblog   url:     https://observablehq.com/@tomlarkworthy/testblog   title:   author:   id:

---
+++ @tomlarkworthy/testblog/ed9e52369f367efd@72.js
@@ -0,0 +1,20 @@
+function _1(md){return(
+md`# Tom's Blog Post
+## Short description goes here`
+)}
+
+function _2(md){return(
+md`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+)}
+
+function _3(md){return(
+md`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+)}
+
+export default function define(runtime, observer) {
+  const main = runtime.module();
+  main.variable(observer()).define(["md"], _1);
+  main.variable(observer()).define(["md"], _2);
+  main.variable(observer()).define(["md"], _3);
+  return main;
+}
---
+++ @tomlarkworthy/testblog/index.js
@@ -0,0 +1,1 @@
+export {default} from "./ed9e52369f367efd@72.js";
