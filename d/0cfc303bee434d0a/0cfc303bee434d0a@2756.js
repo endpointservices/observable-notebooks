198d9662e07c703196265961b9c6df9026aba43e
backup-to-github <robot@webcode.run>
2025-07-07 23:28:29 +0000

    Backup https://observablehq.com/d/0cfc303bee434d0a   url:     https://observablehq.com/d/0cfc303bee434d0a   title:   author:   id:

--- d/0cfc303bee434d0a/0cfc303bee434d0a@2756.js
+++ d/0cfc303bee434d0a/0cfc303bee434d0a@2756.js
@@ -1,5 +1,5 @@
 import define1 from "./f92778131fd76559@1208.js";
-import define2 from "./17c8ce433e1df58e@3332.js";
+import define2 from "./17c8ce433e1df58e@3584.js";
 
 function _1(md){return(
 md`# UUID compressor
