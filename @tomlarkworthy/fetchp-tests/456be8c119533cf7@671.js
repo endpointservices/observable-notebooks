c33abf1cff712cbae504a6c6916a4ffa3eca7981
backup-to-github <robot@webcode.run>
2025-07-08 04:10:24 +0000

    Backup https://observablehq.com/@tomlarkworthy/fetchp-tests   url:     https://observablehq.com/@tomlarkworthy/fetchp-tests   title:   author:   id:

--- @tomlarkworthy/fetchp-tests/456be8c119533cf7@671.js
+++ @tomlarkworthy/fetchp-tests/456be8c119533cf7@671.js
@@ -1,6 +1,6 @@
 import define1 from "./ef672b935bd480fc@623.js";
 import define2 from "./c7a3b20cec5d4dd9@730.js";
-import define3 from "./dff1e917c89f5e76@1964.js";
+import define3 from "./dff1e917c89f5e76@1965.js";
 
 function _1(md){return(
 md`# _fetchp_ tests
