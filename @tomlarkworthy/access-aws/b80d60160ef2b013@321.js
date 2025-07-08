94345c817755c436bdcf3111a9623c056e486415
backup-to-github <robot@webcode.run>
2025-07-08 04:49:26 +0000

    Backup https://observablehq.com/@tomlarkworthy/access-aws   url:     https://observablehq.com/@tomlarkworthy/access-aws   title:   author:   id:

--- @tomlarkworthy/access-aws/b80d60160ef2b013@321.js
+++ @tomlarkworthy/access-aws/b80d60160ef2b013@321.js
@@ -1,5 +1,5 @@
 // https://observablehq.com/@tomlarkworthy/access-aws@321
-import define1 from "./dff1e917c89f5e76@1964.js";
+import define1 from "./dff1e917c89f5e76@1965.js";
 import define2 from "./c7a3b20cec5d4dd9@730.js";
 
 async function _1(md,FileAttachment){return(
