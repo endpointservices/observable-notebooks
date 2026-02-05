3dc68484ff4d287fa8a5cf7af5eff70699fc6af8
backup-to-github <robot@webcode.run>
2026-02-05 08:21:03 +0000

    Backup https://observablehq.com/@tomlarkworthy/serverless-cell-latency-monitor   url:     https://observablehq.com/@tomlarkworthy/serverless-cell-latency-monitor   title:   author:   id:

--- @tomlarkworthy/serverless-cell-latency-monitor/629be1812462d083@415.js
+++ @tomlarkworthy/serverless-cell-latency-monitor/629be1812462d083@415.js
@@ -5,7 +5,7 @@ import define3 from "./11a5ab8b1b3a51db@1161.js";
 import define4 from "./84e66f78139ac354@830.js";
 import define5 from "./a2e58f97fd5e8d7c@756.js";
 import define6 from "./0905542adbad836e@55.js";
-import define7 from "./3d9d1394d858ca97@553.js";
+import define7 from "./3d9d1394d858ca97@556.js";
 import define8 from "./293899bef371e135@293.js";
 
 async function _1(md,FileAttachment){return(
