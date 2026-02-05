08c7dcfdbeb1ad6cf2fc693fcc2693fc35dc4475
backup-to-github <robot@webcode.run>
2026-02-05 07:23:51 +0000

    Backup https://observablehq.com/@tomlarkworthy/blog-firestores-technical-advantages   url:     https://observablehq.com/@tomlarkworthy/blog-firestores-technical-advantages   title:   author:   id:

--- @tomlarkworthy/blog-firestores-technical-advantages/9e9b514f3656a16e@1255.js
+++ @tomlarkworthy/blog-firestores-technical-advantages/9e9b514f3656a16e@1255.js
@@ -4,7 +4,7 @@ import define2 from "./8253abefced00da1@959.js";
 import define3 from "./dff1e917c89f5e76@1965.js";
 import define4 from "./c0de6bf6c2f598ef@62.js";
 import define5 from "./ef672b935bd480fc@623.js";
-import define6 from "./3d9d1394d858ca97@553.js";
+import define6 from "./3d9d1394d858ca97@556.js";
 import define7 from "./b8a500058f806a6b@11.js";
 import define8 from "./293899bef371e135@293.js";
 
